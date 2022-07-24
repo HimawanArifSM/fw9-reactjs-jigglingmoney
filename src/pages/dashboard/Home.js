846c4644f94369ff6ca7217376c918991ac1010b
HimawanArifSM <himawanarif7@gmail.com>
2022-07-24 21:25:31 +0700

    feat: router, modal, responsive(kinda)

--- src/pages/dashboard/Home.js
+++ src/pages/dashboard/Home.js
@@ -162,7 +162,7 @@ function home() {
                 <div class=" d-flex flex-row justify-content-between zwallet-footer mw-100">
                     <div><p5>2020 Zwallet. All right reserved.</p5>
                     </div>
-                    <div>
+                    <div className='d-flex flex-row gap-3'>
                         <p5>+62 5637 8882 9901</p5>
                         <p5>contact@zwallet.com</p5>
                     </div>
