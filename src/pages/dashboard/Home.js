300e6d43d226ee5a51fd8ee84850d3156f399c95
HimawanArifSM <himawanarif7@gmail.com>
2022-07-25 17:00:37 +0700

    fix: fix bug tampilan

--- src/pages/dashboard/Home.js
+++ src/pages/dashboard/Home.js
@@ -37,7 +37,7 @@ function home() {
         <section>
         <Row className='pad-content mw-100'>
             <Col className='col-12 col-md-2 d-flex flex-column justify-content-between sidebar bg-white'>
-            <div className="d-flex flex-column gap-5 ">
+                <div className="d-flex flex-column gap-5 ">
                     <Link to={"/home"} className='align-items-center d-flex gap-1 f400-bck text-decoration-none'>
                         <AiOutlineAppstore />
                         <p5 >Dashboard</p5>
@@ -64,96 +64,96 @@ function home() {
             </Col>
             <Col className='col-10'>
                 <div>
-                <div class="d-flex justify-content-between bg-main-ct pad-sec-1">
-                    <div>
-                        <p>Balance</p>
-                        <h4>Rp120.000</h4>
-                        <p>+62 813-9387-7946</p>
+                    <div class="d-flex justify-content-between bg-main-ct pad-sec-1">
+                        <div>
+                            <p>Balance</p>
+                            <h4>Rp120.000</h4>
+                            <p>+62 813-9387-7946</p>
+                        </div>
+                        <div class="d-flex flex-column gap-2 ">
+                            <a href="/transfer/transfer.html" className="btn blue-button align-items-center d-flex gap-1">
+                                <FiArrowUp />
+                                <p5>Transfer</p5>
+                            </a>
+                            <a href="/topUp/topUp.html" className="btn blue-button align-items-center d-flex gap-1">
+                                <FiPlus />
+                                <p5>Top Up</p5>
+                            </a>
+                        </div>
                     </div>
-                    <div class="d-flex flex-column gap-2 ">
-                        <a href="/transfer/transfer.html" className="btn blue-button align-items-center d-flex gap-1">
-                            <FiArrowUp />
-                            <p5>Transfer</p5>
-                        </a>
-                        <a href="/topUp/topUp.html" className="btn blue-button align-items-center d-flex gap-1">
-                            <FiPlus />
-                            <p5>Top Up</p5>
-                        </a>
-                    </div>
-                </div>
-                <Row className='pad-sec-2 justify-content-between'>
-                    <Col className='col-12 col-md-7 bg-white pad-sec-1'>
-                        <div class="d-flex justify-content-between">
+                    <Row className='pad-sec-2 justify-content-between'>
+                        <Col className='col-12 col-md-7 bg-white pad-sec-1'>
+                            <div class="d-flex justify-content-between">
+                                    <div>
+                                    <FiArrowDown/>
+                                    <p7>Income</p7>
+                                        <p>Rp2.120.000</p>
+                                </div>
                                 <div>
-                                <FiArrowDown/>
-                                <p7>Income</p7>
-                                    <p>Rp2.120.000</p>
-                            </div>
-                            <div>
-                                <FiArrowUp/>
-                                <p7>Expense</p7>
-                                <p>Rp1.560.000</p>
-                            </div>
-                        </div>
-                        <div className='d-flex justify-content-center'>
-                            <img class="img-fluid" src={graph} alt="pict"/>
-                        </div>
-                    </Col>
-                    <Col className='d-flex flex-column col-4 bg-white   pad-sec-1 gap-4 '>
-                        <div class="d-flex justify-content-between">
-                            <p5>Transaction History</p5>
-                            <Link className='f400-bck text-decoration-none' to={"/history"}>See all</Link>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row gap-2">
-                                <img src={contact1} alt="pict"/>
-                                <div class="d-flex flex-column">
-                                    <p5>Samuel Suhi</p5>
-                                    <p9>Transfer</p9>
+                                    <FiArrowUp/>
+                                    <p7>Expense</p7>
+                                    <p>Rp1.560.000</p>
                                 </div>
                             </div>
-                            <div>
-                                <p>+Rp50.000</p>
+                            <div className='d-flex justify-content-center'>
+                                <img class="img-fluid" src={graph} alt="pict"/>
                             </div>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row gap-2">
-                                <img src={contact2} alt="pict"/>
-                                <div class="d-flex flex-column">
-                                    <p5>Netflix</p5>
-                                    <p9>Subscription</p9>
+                        </Col>
+                        <Col className='d-flex flex-column col-4 bg-white   pad-sec-1 gap-4 '>
+                            <div class="d-flex justify-content-between">
+                                <p5>Transaction History</p5>
+                                <Link className='f400-bck text-decoration-none' to={"/history"}>See all</Link>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row gap-2">
+                                    <img src={contact1} alt="pict"/>
+                                    <div class="d-flex flex-column">
+                                        <p5>Samuel Suhi</p5>
+                                        <p9>Transfer</p9>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p>+Rp50.000</p>
                                 </div>
                             </div>
-                            <div>
-                                <p>-Rp149.000</p>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row gap-2">
-                                <img src={contact3} alt="pict"/>
-                                <div class="d-flex flex-column">
-                                    <p5>Christine Martin</p5>
-                                    <p9>Transfer</p9>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row gap-2">
+                                    <img src={contact2} alt="pict"/>
+                                    <div class="d-flex flex-column">
+                                        <p5>Netflix</p5>
+                                        <p9>Subscription</p9>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p>-Rp149.000</p>
                                 </div>
                             </div>
-                            <div>
-                                <p>+Rp150.000</p>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row gap-2a">
-                                <img src={contact4} alt="pict"/>
-                                <div class="d-flex flex-column">
-                                    <p5>Adobe Inc.</p5>
-                                    <p9>Subscription</p9>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row gap-2">
+                                    <img src={contact3} alt="pict"/>
+                                    <div class="d-flex flex-column">
+                                        <p5>Christine Martin</p5>
+                                        <p9>Transfer</p9>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p>+Rp150.000</p>
                                 </div>
                             </div>
-                            <div>
-                                <p>-Rp249.000</p>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row gap-2a">
+                                    <img src={contact4} alt="pict"/>
+                                    <div class="d-flex flex-column">
+                                        <p5>Adobe Inc.</p5>
+                                        <p9>Subscription</p9>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p>-Rp249.000</p>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                </Row>
+                        </Col>
+                    </Row>
                 </div>
             </Col>
         </Row>
@@ -173,3 +173,4 @@ function home() {
 }
 
 export default home
+
