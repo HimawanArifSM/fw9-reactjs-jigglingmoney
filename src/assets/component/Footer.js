c9c00f5bf3cc877fb713d9be86d95201b79525c5
HimawanArifSM <himawanarif7@gmail.com>
2022-07-29 20:43:28 +0700

    feat: responsive

--- src/assets/component/Footer.js
+++ src/assets/component/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div class=" d-flex flex-row justify-content-between zwallet-footer mw-100">
-        <div><p5>2020 Zwallet. All right reserved.</p5>
+    <div class=" d-flex flex-column flex-md-row gap-md-0 gap-3 justify-content-between zwallet-footer mw-100">
+        <div><p3>2020 Zwallet. All right reserved.</p3>
         </div>
-        <div className='d-flex flex-row gap-3'>
-            <p5>+62 5637 8882 9901</p5>
-            <p5>contact@zwallet.com</p5>
+        <div className='d-flex flex-column flex-md-row gap-3'>
+            <p3>+62 5637 8882 9901</p3>
+            <p3>contact@zwallet.com</p3>
         </div>
     </div> 
   )
--- src/assets/component/Sidebar.js
+++ src/assets/component/Sidebar.js
@@ -11,29 +11,29 @@ function Sidebar() {
         navigate('/')
     }
   return (
-    <div className='col-12 col-md-2 d-flex flex-column justify-content-between sidebar bg-white'>
-        <div className="d-flex flex-column gap-5 ">
+    <div className='col-12 col-md-2 d-flex flex-md-column flex-row justify-content-md-between sidebar bg-white mw-md-100 mw-100 gap-5'>
+        <div className="d-flex flex-md-column gap-md-5 gap-5 sb-brake">
             <Link to={"/home"} className='align-items-center d-flex gap-1 f400-bck text-decoration-none'>
                 <AiOutlineAppstore />
-                <p5 >Dashboard</p5>
+                <p5 className='no-disp'>Dashboard</p5>
             </Link>
             <Link to={"/transfer"} className='align-items-center d-flex gap-1 f400-bck text-decoration-none'>
                 <FiArrowUp />
-                <p5>Transfer</p5>
+                <p5 className='no-disp'>Transfer</p5>
             </Link>
             <Link to={"/topup"} className='align-items-center d-flex gap-1 f400-bck text-decoration-none'>
                 <FiPlus />
-                <p5>Top Up</p5>
+                <p5 className='no-disp'>Top Up</p5>
             </Link>
             <Link to={"/profile"} className='align-items-center d-flex gap-1 f400-bck text-decoration-none'>
                 <FiUser />
-                <p5>Profile</p5>
+                <p5 className='no-disp'>Profile</p5>
             </Link>
         </div>
         <div>
             <div type='submit' className='align-items-center d-flex gap-1 f400-bck text-decoration-none ' onClick={onLogout}>
                 <FiLogOut />
-                <p5>Logout</p5>
+                <p5 className='no-disp'>Logout</p5>
             </div>
         </div>
     </div>
